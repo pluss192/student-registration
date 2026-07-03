b503969691d95d25f8973d6f654be56185b1a514
Pluscase_Madoya <pluscasemadoya1@gmail.com>
2026-07-03 10:37:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const SUPABASE_URL = "ecreiwbowkurvckywpgt";
 
+const SUPABASE_ANON_KEY = "sb_publishable_n_Yn3TAXAUgOxxWlz9EhUg_7-zQPPGg";
+
+const client = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
+
+console.log("Supabase Connected!");
